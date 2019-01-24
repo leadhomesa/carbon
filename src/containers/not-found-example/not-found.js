9a927a31fe6d02929d448a34f37d8457b50d266f
Francois Laubscher <francoislaubscher@outlook.com>
2019-01-24 17:38:57 +0200

    svg as react-component plugin

--- src/containers/not-found-example/not-found.js
+++ src/containers/not-found-example/not-found.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+// svgs
+import NotFoundSVG from 'assets/404.svg';
+
 const NotFound = () => (
   <div>
     <h1>This is not the route you&apos;re looking for.</h1>
     <p>This is not the route I&apos;m looking for...</p>
+    <NotFoundSVG />
   </div>
 );
 
