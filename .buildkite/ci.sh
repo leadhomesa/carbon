1a7bcee36e5ecb37f90b78a8242ff1b881a20b8c
Francois Laubscher <francoislaubscher@outlook.com>
2019-06-05 17:29:38 +0200

    DOCKER_TAG

--- .buildkite/ci.sh
+++ .buildkite/ci.sh
@@ -10,7 +10,7 @@ git reset --hard
 # Run gitversion, mounting the repo root folder. Export the result as BUILD_NUMBER
 docker run -v "$(pwd):/repo" gittools/gitversion-dotnetcore:linux /repo
 export BUILD_NUMBER=$(docker run -v "$(pwd):/repo" gittools/gitversion-dotnetcore:linux /repo | jq -r ".LegacySemVer")
-
+export DOCKER_TAG="carbon:${BUILD_NUMBER}"
 echo "+++ :package: Building version ${BUILD_NUMBER}"
 
 # Build docker image
