3119b28da4629344c8a3952bbae01576cdef57a8
Francois Laubscher <francois.laubscher@leadhome.co.za>
2019-07-18 10:03:57 +0200

    cleanup (#15)

--- src/containers/home/style.js
+++ src/containers/home/style.js
@@ -1,12 +1,9 @@
 import styled from 'styled-components';
 import Link from 'react-router-dom/Link';
-import { colors, pxtorem } from 'styles/index-example';
+import { colors } from 'styles/index';
 
 export const StyledLink = styled(Link)`
   display: block;
-  margin-bottom: ${pxtorem(10)};
+  margin-bottom: 10px;
   color: ${colors.coral};
-  :visited {
-    color: ${colors.coral};
-  }
 `;
